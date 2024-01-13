3d37a0afbf2ed53b7d84775a89f73884495eb477
mirodrr <113373534+mirodrr@users.noreply.github.com>
2024-01-12 20:06:38 -0700

    first commit

---
+++ src/lambda.ts
@@ -0,0 +1,34 @@
+import path from 'path';
+import { Duration } from 'aws-cdk-lib';
+import { Role } from 'aws-cdk-lib/aws-iam';
+import { Function, Code, Runtime, Architecture } from 'aws-cdk-lib/aws-lambda';
+import { Construct } from 'constructs';
+
+interface LambdaProps {
+  role: Role;
+}
+export class Lambda extends Construct {
+  public queryLambda: Function;
+
+  constructor(scope: Construct, id: string, props: LambdaProps) {
+    super(scope, id);
+
+    this.queryLambda = new Function(this, 'QueryLambda', {
+      code: Code.fromAsset(path.join(__dirname, 'resources/query_lambda'), {
+        bundling: {
+          image: Runtime.PYTHON_3_9.bundlingImage,
+          command: [
+            'bash',
+            '-c',
+            'pip install -r requirements.txt -t /asset-output && cp -au . /asset-output',
+          ],
+        },
+      }),
+      runtime: Runtime.PYTHON_3_9,
+      architecture: Architecture.X86_64,
+      handler: 'index.handler',
+      timeout: Duration.minutes(5),
+      role: props.role,
+    });
+  }
+}
